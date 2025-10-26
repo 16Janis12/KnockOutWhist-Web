89675a069079085df849b78238e0bc7c3c48a473
Janis <janis.e.20@gmx.de>
2025-10-26 11:24:05 +0100

    chore(base): resolved merge-conflict
    #5 [Story] Create User Sessions

--- knockoutwhistweb/app/logic/user/SessionManager.scala
+++ knockoutwhistweb/app/logic/user/SessionManager.scala
@@ -1,10 +1,7 @@
 package logic.user
 
-import com.google.inject.ImplementedBy
-import logic.user.impl.BaseSessionManager
 import model.users.User
 
-@ImplementedBy(classOf[BaseSessionManager])
 trait SessionManager {
   
   def createSession(user: User): String
--- knockoutwhistweb/app/logic/user/UserManager.scala
+++ knockoutwhistweb/app/logic/user/UserManager.scala
@@ -1,15 +1,10 @@
 package logic.user
 
-import com.google.inject.ImplementedBy
-import logic.user.impl.StubUserManager
-import model.users.User
-
-@ImplementedBy(classOf[StubUserManager])
 trait UserManager {
 
   def addUser(name: String, password: String): Boolean
-  def authenticate(name: String, password: String): Option[User]
-  def userExists(name: String): Option[User]
+  def authenticate(name: String, password: String): Boolean
+  def userExists(name: String): Boolean
   def removeUser(name: String): Boolean
 
 }
